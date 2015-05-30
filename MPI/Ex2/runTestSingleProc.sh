57d71eae336c6db6a55a8cc6ece20ff524288f52
Parisbre56 <parisbre56@users.noreply.github.com>
2015-05-31 01:51:23 +0300

    new branch, new data

--- MPI/Ex2/runTestSingleProc.sh
+++ MPI/Ex2/runTestSingleProc.sh
@@ -32,8 +32,8 @@ function conduct_test() {
 	for w1 in $3; do
 		for w2 in $4; do #for all omegaRed and omegaBlack and q and dataSize
 			for sqrtN in $5; do
-				>&2 echo "g++ -O3 -std=c++11 -D SINGLE_PROC -D NDEBUG -D \"omegaRed=$w1\" -D \"omegaBlack=$w2\" -D \"sqrtN=$sqrtN\" -o ./Ex2 ./main.cpp"
-				g++ -O3 -std=c++11 -D NDEBUG -D SINGLE_PROC -D "omegaRed=$w1" -D "omegaBlack=$w2" -D "sqrtN=$sqrtN" -o ./Ex2 ./main.cpp
+				>&2 echo "g++ -O3 -std=c++11 -D SINGLE_PROC -D NDEBUG -D \"omegaRed=$w1\" -D \"omegaBlack=$w2\" -D \"sqrtN=$sqrtN\" -D \"ITER_CHECK=10\" -o ./Ex2 ./main.cpp"
+				g++ -O3 -std=c++11 -D NDEBUG -D SINGLE_PROC -D "omegaRed=$w1" -D "omegaBlack=$w2" -D "sqrtN=$sqrtN" -D "ITER_CHECK=10" -o ./Ex2 ./main.cpp
 				t=0.0
 				for j in $(seq 1 $1); do #execute the thing 20 times and use sum/20 to find the average time
 					>&2 echo -n "$w1:$w2:$sqrtN:1:$j "
@@ -70,7 +70,8 @@ function conduct_test() {
 
 tests=5;
 w1s='0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9'
-w2s='0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9'
+#the 00001 is so that the compiler does not optimise the two equal values to make computations faster
+w2s='0.100001 0.200001 0.300001 0.400001 0.500001 0.600001 0.700001 0.800001 0.900001 1.000001 1.100001 1.200001 1.300001 1.400001 1.500001 1.600001 1.700001 1.800001 1.900001'
 sqrtNs='5 10 20 40 60 100 150'
 
 conduct_test $tests "" "$w1s" "$w2s" "$sqrtNs"
